7ff4b695555c995be0e69d762f4c5e2c012905cf
Christie Beaubrun <christiebeaubrun02@gmail.com>
2023-11-11 02:44:36 -0500

    app

---
+++ ActiveVillage/ActiveVillage/CreateAccoutViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CreateAccoutViewController.swift
+//  ActiveVillage
+//
+//  Created by Christie beaubrun on 11/11/23.
+//
+
+import UIKit
+import Firebase
+
+class CreateAccoutViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signupClicked(_ sender: UIButton) 
+    {
+        guard let email = emailTextfield.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error{
+                print("Error, Try again")
+            }
+            else{
+                self.performSegue(withIdentifier: "goToNext", sender: self)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
